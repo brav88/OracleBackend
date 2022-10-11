02ba604079ffbe68cf40a2bdaf34c11639bfa55b
brav88 <brav88@hotmail.com>
2022-10-10 21:08:35 -0600

    Agregar archivos de proyecto.

---
+++ OracleBackend/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OracleBackend.Models;
+using System.Data;
+using System.Diagnostics;
+
+namespace OracleBackend.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            //DatabaseHelper.OracleDatabase.InsertPerson("Jason", "Taylor");
+            //DatabaseHelper.OracleDatabase.DeletePerson(21);
+            //DatabaseHelper.OracleDatabase.UpdatePerson("Travis", "Scott", 7);
+            DataTable ds = DatabaseHelper.OracleDatabase.GetPersons();
+
+            List<Person> persons = new List<Person>();
+            foreach (DataRow item in ds.Rows)
+            {
+                persons.Add(new Person
+                {
+                    FirstName = item["first_name"].ToString(),
+                    LastName = item["last_name"].ToString()
+                });
+            }
+
+            ViewBag.Persons = persons;
+
+            return View();
+        }
+
+        public class Person
+        {
+            public string? FirstName { get; set; }
+            public string? LastName { get; set; }
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ OracleBackend/DatabaseHelper/OracleDatabase.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.AspNetCore.Hosting.Server;
+using Oracle.ManagedDataAccess.Client;
+using System.Data;
+
+namespace OracleBackend.DatabaseHelper
+{
+    public class OracleDatabase
+    {
+        static string strConexion = "user id=CWEB;password=Oracle123;data source=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)" +
+                          "(HOST=localhost)(PORT=1521))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=orcl)))";
+
+        public static void InsertPerson(string firstName, string lastName)
+        {
+            List<OracleParameter> param = new List<OracleParameter>() {
+                new OracleParameter("@first_name", firstName),
+                new OracleParameter("@last_name", lastName)
+            };
+
+            ExecStoreProcedure("CWEB.PckgPersons.InsertPerson", param);
+        }
+
+        public static void DeletePerson(int personid)
+        {
+            List<OracleParameter> param = new List<OracleParameter>() {
+                new OracleParameter("@firstName", personid)
+            };
+
+            ExecStoreProcedure("CWEB.PckgPersons.DeletePerson", param);
+        }
+
+        public static void UpdatePerson(string firstName, string lastName, int personid)
+        {
+            List<OracleParameter> param = new List<OracleParameter>() {
+                new OracleParameter("@firstName", firstName),
+                new OracleParameter("@lastName", lastName),
+                new OracleParameter("@personid", personid)
+            };
+
+            ExecStoreProcedure("CWEB.PckgPersons.UpdatePerson", param);
+        }
+
+        public static DataTable GetPersons()
+        {
+            List<OracleParameter> param = new List<OracleParameter>() {
+                new OracleParameter("@personsCursor", OracleDbType.RefCursor, ParameterDirection.InputOutput)
+            };
+
+            return ExecuteStoreProcedure("CWEB.PckgPersons.GetPersons", param);
+        }
+
+
+        //Para select 
+        public static DataTable ExecuteStoreProcedure(string procedure, List<OracleParameter> param)
+        {
+            try
+            {
+                using (OracleConnection conn = new OracleConnection(strConexion))
+                {
+                    conn.Open();
+                    OracleCommand cmd = new OracleCommand();
+                    cmd.CommandText = procedure;
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Connection = conn;
+
+                    if (param != null)
+                    {
+                        foreach (OracleParameter item in param)
+                        {
+                            cmd.Parameters.Add(item);
+                        }
+                    }
+
+                    cmd.ExecuteNonQuery();
+                    OracleDataAdapter adapter = new OracleDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    adapter.Fill(dt);
+
+                    return dt;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public static void ExecStoreProcedure(string procedure, List<OracleParameter> param)
+        {
+            try
+            {
+                using (OracleConnection conn = new OracleConnection(strConexion))
+                {
+                    conn.Open();
+                    OracleCommand cmd = new OracleCommand();
+                    cmd.CommandText = procedure;
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Connection = conn;
+
+                    if (param != null)
+                    {
+                        foreach (OracleParameter item in param)
+                        {
+                            cmd.Parameters.Add(item);
+                        }
+                    }
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
